bf731a60037e034cae18d92ff8f2e3a157f78108
Shekn <cgproggame@gmail.com>
2022-04-21 21:01:12 +0500

    Export shaderball camera

--- render_lux/lux_scene/lux_camera.cpp
+++ render_lux/lux_scene/lux_camera.cpp
@@ -3,7 +3,17 @@
 
 #include "xsi_kinematics.h"
 
-void sync_camera(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI::CTime& eval_time)
+void set_lux_camera(luxcore::Scene* scene, const float fov, const XSI::MATH::CVector3 &xsi_position, const XSI::MATH::CVector3 &xsi_target_position)
+{
+	std::array<float, 3> lux_position = xsi_to_lux_vector(xsi_position);
+	std::array<float, 3> lux_target_position = xsi_to_lux_vector(xsi_target_position);
+	scene->Parse(
+		luxrays::Property("scene.camera.lookat.orig")(lux_position[0], lux_position[1], lux_position[2]) <<
+		luxrays::Property("scene.camera.lookat.target")(lux_target_position[0], lux_target_position[1], lux_target_position[2]) <<
+		luxrays::Property("scene.camera.fieldofview")(fov));
+}
+
+void sync_camera_scene(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI::CTime& eval_time)
 {
 	XSI::MATH::CTransformation xsi_tfm = xsi_camera.GetKinematics().GetGlobal().GetTransform();
 	XSI::MATH::CVector3 xsi_position = xsi_tfm.GetTranslation();
@@ -21,10 +31,16 @@ void sync_camera(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI
 	float fov = xsi_camera.GetParameterValue("fov", eval_time);
 
 	//set lux camera
-	std::array<float, 3> lux_position = xsi_to_lux_vector(xsi_position);
-	std::array<float, 3> lux_target_position = xsi_to_lux_vector(xsi_target_position);
-	scene->Parse(
-		luxrays::Property("scene.camera.lookat.orig")(lux_position[0], lux_position[1], lux_position[2]) <<
-		luxrays::Property("scene.camera.lookat.target")(lux_target_position[0], lux_target_position[1], lux_target_position[2]) <<
-		luxrays::Property("scene.camera.fieldofview")(fov));
+	set_lux_camera(scene, fov, xsi_position, xsi_target_position);
+}
+
+void sync_camera_shaderball(luxcore::Scene* scene)
+{
+	XSI::MATH::CVector3 xsi_position = XSI::MATH::CVector3(15.68, 5.89, 0.0);
+	XSI::MATH::CVector3 xsi_direction = XSI::MATH::CVector3(-0.94, -0.34, 0.0);
+
+	XSI::MATH::CVector3 xsi_target_position = XSI::MATH::CVector3(xsi_position.GetX() + xsi_direction.GetX(),
+																  xsi_position.GetY() + xsi_direction.GetY(),
+																  xsi_position.GetZ() + xsi_direction.GetZ());
+	set_lux_camera(scene, 28.94f, xsi_position, xsi_target_position);
 }
--- render_lux/lux_scene/lux_scene.h
+++ render_lux/lux_scene/lux_scene.h
@@ -4,4 +4,5 @@
 #include "xsi_camera.h"
 #include "xsi_time.h"
 
-void sync_camera(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI::CTime& eval_time);
+void sync_camera_scene(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI::CTime& eval_time);
+void sync_camera_shaderball(luxcore::Scene* scene);

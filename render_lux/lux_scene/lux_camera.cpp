4ac7fa0f566894dc1563b359b1774b0b37a7d0ae
Shekn <cgproggame@gmail.com>
2022-04-22 17:11:03 +0500

    Upgrade camera export
    Properly calculate fov in horisontal and vertical modes

--- render_lux/lux_scene/lux_camera.cpp
+++ render_lux/lux_scene/lux_camera.cpp
@@ -1,16 +1,19 @@
 #include "lux_scene.h"
 #include "../../utilities/math.h"
+#include "../../utilities/logs.h"
 
 #include "xsi_kinematics.h"
 
-void set_lux_camera(luxcore::Scene* scene, const float fov, const XSI::MATH::CVector3 &xsi_position, const XSI::MATH::CVector3 &xsi_target_position)
+#define M_PI 3.14159265358979323846
+#define DEG2RADF(_deg) ((_deg) * (float)(M_PI / 180.0))
+#define RAD2DEGF(_rad) ((_rad) * (float)(180.0 / M_PI))
+
+void set_lux_camera_positions(luxrays::Properties &camera_props, const XSI::MATH::CVector3& xsi_position, const XSI::MATH::CVector3& xsi_target_position)
 {
 	std::array<float, 3> lux_position = xsi_to_lux_vector(xsi_position);
 	std::array<float, 3> lux_target_position = xsi_to_lux_vector(xsi_target_position);
-	scene->Parse(
-		luxrays::Property("scene.camera.lookat.orig")(lux_position[0], lux_position[1], lux_position[2]) <<
-		luxrays::Property("scene.camera.lookat.target")(lux_target_position[0], lux_target_position[1], lux_target_position[2]) <<
-		luxrays::Property("scene.camera.fieldofview")(fov));
+	camera_props.Set(luxrays::Property("scene.camera.lookat.orig")(lux_position[0], lux_position[1], lux_position[2]));
+	camera_props.Set(luxrays::Property("scene.camera.lookat.target")(lux_target_position[0], lux_target_position[1], lux_target_position[2]));
 }
 
 void sync_camera_scene(luxcore::Scene* scene, const XSI::Camera& xsi_camera, const XSI::CTime& eval_time)
@@ -29,12 +32,45 @@ void sync_camera_scene(luxcore::Scene* scene, const XSI::Camera& xsi_camera, con
 	}
 
 	float fov = xsi_camera.GetParameterValue("fov", eval_time);
+	int fov_type = xsi_camera.GetParameterValue("fovtype", eval_time);
+	if (fov_type == 0)
+	{
+		//recalculate fov from horizontal to vertical
+		float aspect = xsi_camera.GetParameterValue("aspect", eval_time);
+		fov = RAD2DEGF(2 * atan(tan(DEG2RADF(fov) / 2.0) * aspect));
+	}
+	int xsi_ortho_mode = xsi_camera.GetParameterValue("proj", eval_time);  // 0 - orthographic, 1 - perspective
 
-	//set lux camera
-	set_lux_camera(scene, fov, xsi_position, xsi_target_position);
+	luxrays::Properties camera_props;
+	camera_props.Set(luxrays::Property("scene.camera.type")(xsi_ortho_mode == 1 ? "perspective" : "orthographic"));
+	//TODO: orthographic mode works incorrectly
+	set_lux_camera_positions(camera_props, xsi_position, xsi_target_position);
+	camera_props.Set(luxrays::Property("scene.camera.fieldofview")(fov));
+
+	//up vector
+	//TODO: can not understand, how to properly setup it
+	//XSI::MATH::CMatrix4 xsi_tfm_matrix = xsi_tfm.GetMatrix4();
+	//camera_props.Set(luxrays::Property("scene.camera.up")(xsi_tfm_matrix.GetValue(0, 1), xsi_tfm_matrix.GetValue(1, 1), xsi_tfm_matrix.GetValue(2, 1)));
+
+	//clipping planes
+	float near_clip = xsi_camera.GetParameterValue("near", eval_time);
+	float far_clip = xsi_camera.GetParameterValue("far", eval_time);
+	camera_props.Set(luxrays::Property("scene.camera.cliphither")(near_clip));
+	camera_props.Set(luxrays::Property("scene.camera.clipyon")(far_clip));
+
+	//set default for motion blur
+	camera_props.Set(luxrays::Property("scene.camera.shutteropen")(0));
+	camera_props.Set(luxrays::Property("scene.camera.shutterclose")(0));
+	camera_props.Set(luxrays::Property("scene.camera.lensradius")(0));
+	camera_props.Set(luxrays::Property("scene.camera.focaldistance")(get_distance(xsi_position, xsi_target_position)));
+	camera_props.Set(luxrays::Property("scene.camera.autofocus.enable")(0));
+
+	//TODO: look at parsecamera.cpp source file to find all camera attributes
+
+	scene->Parse(camera_props);
 }
 
-void sync_camera_shaderball(luxcore::Scene* scene)
+void sync_camera_shaderball(luxcore::Scene * scene)
 {
 	XSI::MATH::CVector3 xsi_position = XSI::MATH::CVector3(15.68, 5.89, 0.0);
 	XSI::MATH::CVector3 xsi_direction = XSI::MATH::CVector3(-0.94, -0.34, 0.0);
@@ -42,5 +78,11 @@ void sync_camera_shaderball(luxcore::Scene* scene)
 	XSI::MATH::CVector3 xsi_target_position = XSI::MATH::CVector3(xsi_position.GetX() + xsi_direction.GetX(),
 																  xsi_position.GetY() + xsi_direction.GetY(),
 																  xsi_position.GetZ() + xsi_direction.GetZ());
-	set_lux_camera(scene, 28.94f, xsi_position, xsi_target_position);
+	luxrays::Properties camera_props;
+	camera_props.Set(luxrays::Property("scene.camera.type")("perspective"));
+	set_lux_camera_positions(camera_props, xsi_position, xsi_target_position);
+	camera_props.Set(luxrays::Property("scene.camera.fieldofview")(28.94f));
+	camera_props.Set(luxrays::Property("scene.camera.up")(0.0, 0.0, 1.0));
+
+	scene->Parse(camera_props);
 }
